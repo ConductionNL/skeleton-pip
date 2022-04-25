4c8589b46ba95e47c5590bb5de910f40b5eafe70
Lennart <66552932+lencodes@users.noreply.github.com>
2022-04-25 14:44:34 +0200

    CLEANUP

--- pwa/src/templates/landing/LandingTemplate.tsx
+++ pwa/src/templates/landing/LandingTemplate.tsx
@@ -1,13 +1,6 @@
-import { Button, Card } from "@gemeente-denhaag/components-react";
+import { Button, Card, Link } from "@gemeente-denhaag/components-react";
 import * as React from "react";
 
 export const LandingTemplate: React.FC = () => {
-  return (
-    <div className="LandingTemplate">
-      Landing Template
-      <Button variant="primary-action">Primary</Button>
-      <Button variant="secondary-action">Secondary</Button>
-      <Card title="Title" variant="case" subTitle="Subtitle"></Card>
-    </div>
-  );
+  return <div className="LandingTemplate">Landing Template</div>;
 };
