404528f1db4684d784361c685e516ee2d57484d7
Lennart <66552932+lencodes@users.noreply.github.com>
2022-04-26 12:40:07 +0200

    ADD Sidenav to DashboardTemplate

--- pwa/src/pages/index.tsx
+++ pwa/src/pages/index.tsx
@@ -2,9 +2,16 @@ import * as React from "react";
 import { isLoggedIn } from "../services/auth";
 import { DashboardTemplate } from "../templates/dashboard/DashboardTemplate";
 import { LandingTemplate } from "../templates/landing/LandingTemplate";
+import { OverviewTemplate } from "../templates/templateParts/overview/OverviewTemplate";
 
 const IndexPage: React.FC = () => {
-  return isLoggedIn() ? <DashboardTemplate /> : <LandingTemplate />;
+  return isLoggedIn() ? (
+    <DashboardTemplate>
+      <OverviewTemplate />
+    </DashboardTemplate>
+  ) : (
+    <LandingTemplate />
+  );
 };
 
 export default IndexPage;
