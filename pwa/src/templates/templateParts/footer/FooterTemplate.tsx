44f585462e835346a45dfad3837d3389d7add95b
Barry Brands <barrybrands02@hotmail.com>
2022-04-26 15:58:12 +0200

    imageDivider added

--- pwa/src/templates/templateParts/footer/FooterTemplate.tsx
+++ pwa/src/templates/templateParts/footer/FooterTemplate.tsx
@@ -3,25 +3,29 @@ import "./FooterTemplate.css";
 import { Container } from "../../../components/container/Container";
 import { List, ListItem, ListSubheader } from "@gemeente-denhaag/list";
 import { navigate } from "gatsby";
+import { ImageDivider } from "../../../components/imageDivider/imageDivider";
 
 export const FooterTemplate: React.FC = () => {
   return (
-    <footer className="Footer">
-      <Container>
-        <div className="Footer-inner">
-          <List>
-            <ListSubheader>The Hague</ListSubheader>
-            <ListItem primaryText="Go to denhaag.nl" actionType="nav" onClick={() => navigate("#")} />
-          </List>
+    <>
+      <footer className="Footer">
+        <ImageDivider />
+        <Container>
+          <div className="Footer-inner">
+            <List>
+              <ListSubheader>The Hague</ListSubheader>
+              <ListItem primaryText="Go to denhaag.nl" actionType="nav" onClick={() => navigate("#")} />
+            </List>
 
-          <List>
-            <ListSubheader>Disclaimers</ListSubheader>
-            <ListItem primaryText="Accessibility declaration" actionType="nav" onClick={() => navigate("#")} />
-            <ListItem primaryText="Data Protection declaration" actionType="nav" onClick={() => navigate("#")} />
-            <ListItem primaryText="Proclaimer" actionType="nav" onClick={() => navigate("#")} />
-          </List>
-        </div>
-      </Container>
-    </footer>
+            <List>
+              <ListSubheader>Disclaimers</ListSubheader>
+              <ListItem primaryText="Accessibility declaration" actionType="nav" onClick={() => navigate("#")} />
+              <ListItem primaryText="Data Protection declaration" actionType="nav" onClick={() => navigate("#")} />
+              <ListItem primaryText="Proclaimer" actionType="nav" onClick={() => navigate("#")} />
+            </List>
+          </div>
+        </Container>
+      </footer>
+    </>
   );
 };
