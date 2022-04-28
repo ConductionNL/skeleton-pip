e8c26f6612b2b94d0ed0618f08dd6942eb8c4846
Hamid <gh.hamid89@gmail.com>
2022-04-28 14:54:17 +0200

    LandingTemplate op bouwen

--- pwa/src/templates/landing/LandingTemplate.tsx
+++ pwa/src/templates/landing/LandingTemplate.tsx
@@ -1,5 +1,29 @@
 import * as React from "react";
-
+import { Container } from "../../components/container/Container";
+import { Card } from "@gemeente-denhaag/components-react";
+import "./LandingTemplate.css";
+import { navigate } from "gatsby";
 export const LandingTemplate: React.FC = () => {
-  return <div className="LandingTemplate">Landing Template</div>;
+  return (
+    <Container>
+      <div className="LandingTemplate">
+        <div className="LandingTemplate-header">
+          <h1>Inloggen bij MijnDenHaag</h1>
+          <p>
+            In de persoonlijke online omgeving MijnDenHaag kunt u makkelijk, snel en veilig zaken regelen bij de
+            gemeente. Bewoners kunnen bijvoorbeeld een verhuizing doorgeven of extra parkeertegoed kopen via DigiD.
+            Bedrijven kunnen eenvoudig online zaken regelen via eHerkenning.
+          </p>
+        </div>
+        <div className="LandingTemplate-body">
+          <Card
+            className={"LandingTemplate-body-card"}
+            title={"Login"}
+            onClick={() => navigate("/login")}
+            subTitle={"Account"}
+          />
+        </div>
+      </div>
+    </Container>
+  );
 };
