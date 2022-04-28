e56f6bacbb16c3fd01048edd616ee28857d9dc74
Barry Brands <barrybrands02@hotmail.com>
2022-04-28 11:06:09 +0200

    Form added

--- pwa/src/templates/templateParts/forms/FormsTemplate.tsx
+++ pwa/src/templates/templateParts/forms/FormsTemplate.tsx
@@ -1,4 +1,22 @@
 import * as React from "react";
 import { Heading1 } from "@gemeente-denhaag/components-react";
+import basicForm from "../../../dummy_data/form";
 
-export const FormsTemplate: React.FC = () => <Heading1>Forms</Heading1>;
+export const FormsTemplate: React.FC = () => {
+  const [formIO, setFormIO] = React.useState(null);
+
+  React.useEffect(() => {
+    if (formIO) return;
+    import("@formio/react").then((formio) => {
+      const { Form } = formio;
+      setFormIO(<Form form={basicForm} />);
+    });
+  }, [formIO]);
+
+  return (
+    <>
+      <Heading1>Forms</Heading1>
+      {formIO && formIO}
+    </>
+  );
+};
