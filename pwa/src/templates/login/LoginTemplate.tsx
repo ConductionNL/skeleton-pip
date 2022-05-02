274b5a2c102f15cb38ca0000ed40ed061cae28d9
Remko <remko@conduction.nl>
2022-05-02 15:38:39 +0200

    Changed LoginTemplate css file

--- pwa/src/templates/login/LoginTemplate.tsx
+++ pwa/src/templates/login/LoginTemplate.tsx
@@ -1,16 +1,16 @@
-import { Heading2 } from "@gemeente-denhaag/components-react";
 import * as React from "react";
+import * as styles from "./LoginTemplate.module.css";
+import { Heading2 } from "@gemeente-denhaag/components-react";
 import { useTranslation } from "react-i18next";
 import { LoginForm } from "../../components/loginForm/LoginForm";
-import "./LoginTemplate.css";
 
 export const LoginTemplate: React.FC = () => {
   const { t } = useTranslation();
 
   return (
-    <div className="LoginTemplate">
-      <div className="LoginTemplate-inner">
-        <Heading2 className="LoginTemplate-inner-heading">{t("Login")}</Heading2>
+    <div className={styles.container}>
+      <div className={styles.grid}>
+        <Heading2 className={styles.header}>{t("Login")}</Heading2>
         <LoginForm />
       </div>
     </div>
