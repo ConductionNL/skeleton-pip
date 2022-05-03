d0b98f2b2fe2e2ee286743b9dbcd8142b3355812
Lennart <66552932+lencodes@users.noreply.github.com>
2022-05-03 13:26:23 +0200

    REFACTOR after PR FEEDBACK

--- pwa/src/templates/templateParts/myAccount/MyAccountTemplate.tsx
+++ pwa/src/templates/templateParts/myAccount/MyAccountTemplate.tsx
@@ -1,168 +1,137 @@
 import * as React from "react";
-import { Heading2, Heading3, Link, Button, Paragraph } from "@gemeente-denhaag/components-react";
-import { Table, TableBody, TableRow, TableCell } from "@gemeente-denhaag/table";
+import * as styles from "./MyAccountTemplate.module.css";
+import { Heading1, Heading3, Link, Button, Paragraph } from "@gemeente-denhaag/components-react";
+import { Table, TableBody, TableRow, TableCell, TableHead, TableHeader } from "@gemeente-denhaag/table";
 import { EditIcon } from "@gemeente-denhaag/icons";
-import "./MyAccountTemplate.css";
 import { useTranslation } from "react-i18next";
 
 export const MyAccountTemplate: React.FC = () => {
   const { t } = useTranslation();
 
   return (
-    <div className="MyAccountTemplate">
-      <Heading2 className="MyAccountTemplate-heading2">{t("My account")}</Heading2>
-      <div className="MyAccountTemplate-spacing">
-        <Heading3 className="MyAccountTemplate-heading3">Contact</Heading3>
+    <div className={styles.container}>
+      <Heading1 className={styles.heading}>{t("My account")}</Heading1>
+
+      <div className={styles.block}>
+        <Heading3>{t("Contact")}</Heading3>
+
         <Table>
           <TableBody>
             <TableRow>
-              <TableCell className="MyAccountTemplate-th">
-                <b>Email address</b>
-              </TableCell>
-              <TableCell>test@gateway.local</TableCell>
+              <TableHeader className={styles.th}>{t("Email address")}</TableHeader>
+              <TableCell>john@doe.com</TableCell>
               <TableCell>
-                <Link>
-                  <EditIcon /> Edit
+                <Link icon={<EditIcon />} iconAlign="start">
+                  {t("Edit")}
                 </Link>
               </TableCell>
             </TableRow>
             <TableRow>
-              <TableCell className="MyAccountTemplate-th">
-                <b>Phone number</b>
-              </TableCell>
-              <TableCell>-</TableCell>
+              <TableHeader className={styles.th}>{t("Phone number")}</TableHeader>
+              <TableCell>06 000 000 00</TableCell>
               <TableCell>
-                <Link>
-                  <EditIcon /> Edit
+                <Link icon={<EditIcon />} iconAlign="start">
+                  {t("Edit")}
                 </Link>
               </TableCell>
             </TableRow>
           </TableBody>
         </Table>
       </div>
-      <div className="MyAccountTemplate-spacing">
-        <Heading3 className="MyAccountTemplate-heading3">Notifications and updates</Heading3>
+
+      <div className={styles.block}>
+        <Heading3>{t("Notifications and updates")}</Heading3>
+
         <Table>
           <TableBody>
             <TableRow>
-              <TableCell className="MyAccountTemplate-th">
-                <b>Case updates</b>
-              </TableCell>
-              <TableCell>-</TableCell>
-              <TableCell></TableCell>
+              <TableHeader className={styles.th}>{t("Case updates")}</TableHeader>
+              <TableCell>Update</TableCell>
             </TableRow>
             <TableRow>
-              <TableCell className="MyAccountTemplate-th">
-                <b>News about my neighbourhood </b>
-              </TableCell>
-              <TableCell>-</TableCell>
-              <TableCell></TableCell>
+              <TableHeader className={styles.th}>{t("News about my neighbourhood")}</TableHeader>
+              <TableCell>News</TableCell>
             </TableRow>
             <TableRow>
-              <TableCell className="MyAccountTemplate-th">
-                <b>Tips</b>
-              </TableCell>
-              <TableCell>-</TableCell>
-              <TableCell></TableCell>
+              <TableHeader className={styles.th}>{t("Tips")}</TableHeader>
+              <TableCell>Tips</TableCell>
             </TableRow>
           </TableBody>
         </Table>
       </div>
-      <div className="MyAccountTemplate-spacing">
-        <Heading3 className="MyAccountTemplate-heading3">Personal details</Heading3>
+
+      <div className={styles.block}>
+        <Heading3>{t("Personal details")}</Heading3>
+
         <Table>
           <TableBody>
             <TableRow>
-              <TableCell className="MyAccountTemplate-th">
-                <b>First names</b>
-              </TableCell>
-              <TableCell>test@gateway.local</TableCell>
-              <TableCell></TableCell>
+              <TableHeader className={styles.th}>{t("First names")}</TableHeader>
+              <TableCell>Urbain Achilles Anna Maria</TableCell>
             </TableRow>
             <TableRow>
-              <TableCell className="MyAccountTemplate-th">
-                <b>Last name</b>
-              </TableCell>
-              <TableCell>-</TableCell>
-              <TableCell></TableCell>
+              <TableHeader className={styles.th}>{t("Last name")}</TableHeader>
+              <TableCell>Zijda</TableCell>
             </TableRow>
             <TableRow>
-              <TableCell className="MyAccountTemplate-th">
-                <b>Gender</b>
-              </TableCell>
-              <TableCell>-</TableCell>
-              <TableCell></TableCell>
+              <TableHeader className={styles.th}>{t("Gender")}</TableHeader>
+              <TableCell>man</TableCell>
             </TableRow>
             <TableRow>
-              <TableCell className="MyAccountTemplate-th">
-                <b>Citizen service number</b>
-              </TableCell>
-              <TableCell>-</TableCell>
-              <TableCell></TableCell>
+              <TableHeader className={styles.th}>{t("Citizen service number")}</TableHeader>
+              <TableCell>900050287</TableCell>
             </TableRow>
             <TableRow>
-              <TableCell className="MyAccountTemplate-th">
-                <b>Date of birth</b>
-              </TableCell>
-              <TableCell>10 April 2022</TableCell>
-              <TableCell></TableCell>
+              <TableHeader className={styles.th}>{t("Date of birth")}</TableHeader>
+              <TableCell>6 juni 1967</TableCell>
             </TableRow>
             <TableRow>
-              <TableCell className="MyAccountTemplate-th">
-                <b>Place of birth</b>
-              </TableCell>
-              <TableCell>localhost</TableCell>
-              <TableCell></TableCell>
+              <TableHeader className={styles.th}>{t("Place of birth")}</TableHeader>
+              <TableCell>Pakistan</TableCell>
             </TableRow>
             <TableRow>
-              <TableCell className="MyAccountTemplate-th">
-                <b>Nationality</b>
-              </TableCell>
-              <TableCell>-</TableCell>
-              <TableCell></TableCell>
+              <TableHeader className={styles.th}>{t("Nationality")}</TableHeader>
+              <TableCell>Nederlandse</TableCell>
             </TableRow>
           </TableBody>
         </Table>
       </div>
-      <div className="MyAccountTemplate-spacing">
-        <Heading3 className="MyAccountTemplate-heading3">Address</Heading3>
+
+      <div className={styles.block}>
+        <Heading3>{t("Address")}</Heading3>
+
         <Table>
           <TableBody>
             <TableRow>
-              <TableCell className="MyAccountTemplate-th">
-                <b>Street</b>
-              </TableCell>
-              <TableCell>-</TableCell>
-              <TableCell></TableCell>
+              <TableHeader className={styles.th}>Street</TableHeader>
+              <TableCell>Gagelplein 5</TableCell>
             </TableRow>
             <TableRow>
-              <TableCell className="MyAccountTemplate-th">
-                <b>Postal code and city</b>
-              </TableCell>
-              <TableCell>-</TableCell>
-              <TableCell></TableCell>
+              <TableHeader className={styles.th}>{t("Postal code and city")}</TableHeader>
+              <TableCell>2563TT Den Haag</TableCell>
             </TableRow>
           </TableBody>
         </Table>
       </div>
-      <div className="MyAccountTemplate-spacing">
-        <Heading3 className="MyAccountTemplate-heading3">Number of inhabitants address</Heading3>
+
+      <div className={styles.block}>
+        <Heading3>{t("Number of inhabitants address")}</Heading3>
+
         <Table>
           <TableBody>
             <TableRow>
-              <TableCell className="MyAccountTemplate-th">
-                <b>Number of people on your address</b>
-              </TableCell>
-              <TableCell>-</TableCell>
-              <TableCell></TableCell>
+              <TableHeader className={styles.th}>{t("Number of people on your address")}</TableHeader>
+              <TableCell>3</TableCell>
             </TableRow>
           </TableBody>
         </Table>
-        <p>
-          Is this not the number of people you expect? You can request an address investigation with your municipility.
-          This investigation can also be cancelled.
-        </p>
-        <Button>Request research</Button>
+
+        <Paragraph>
+          {t(
+            "Is this not the number of people you expect? You can request an address investigation with your municipility. This investigation can also be cancelled.",
+          )}
+        </Paragraph>
+        <Button>{t("Request research")}</Button>
       </div>
     </div>
   );
