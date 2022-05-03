98623b420a4a3417a0aa300c8d0e9ae68c793941
Remko <remko@conduction.nl>
2022-05-03 12:17:23 +0200

    Container

--- pwa/src/components/container/Container.tsx
+++ pwa/src/components/container/Container.tsx
@@ -1,4 +1,4 @@
 import * as React from "react";
-import "./Container.css";
+import * as styles from "./Container.module.css";
 
-export const Container: React.FC = ({ children }) => <div className="Container">{children}</div>;
+export const Container: React.FC = ({ children }) => <div className={styles.container}>{children}</div>;
