88cfe9bd83799bcd3285179ee1822513cc84818c
Lennart <66552932+lencodes@users.noreply.github.com>
2022-05-05 09:51:22 +0200

    IMPLEMENT MessagesTable in overview template

--- pwa/src/templates/templateParts/overview/OverviewTemplate.tsx
+++ pwa/src/templates/templateParts/overview/OverviewTemplate.tsx
@@ -1,10 +1,11 @@
 import * as React from "react";
 import * as styles from "./OverviewTemplate.module.css";
-import { Divider, Heading1, Heading3, Link, Tab, TabContext, TabPanel, Tabs } from "@gemeente-denhaag/components-react";
+import { Heading1, Heading3, Link, Tab, TabContext, TabPanel, Tabs } from "@gemeente-denhaag/components-react";
 import { ArrowRightIcon } from "@gemeente-denhaag/icons";
 import { useTranslation } from "react-i18next";
 import { navigate } from "gatsby";
 import { CasesTable, ICaseTableItem } from "../../../components/casesTable/CasesTable";
+import { IMessageTableItem, MessagesTable } from "../../../components/messagesTable/MessagesTable";
 
 export const OverviewTemplate: React.FC = () => {
   const { t } = useTranslation();
@@ -20,7 +21,7 @@ export const OverviewTemplate: React.FC = () => {
         <div className={styles.messagesHeading}>
           <Heading3>{t("My messages")}</Heading3>
 
-          <div onClick={() => navigate("/my-cases")}>
+          <div onClick={() => navigate("/my-messages")}>
             <Link icon={<ArrowRightIcon />} iconAlign="end">
               {t("Show all messages")}
             </Link>
@@ -39,12 +40,14 @@ export const OverviewTemplate: React.FC = () => {
             <Tab label={t("Read messages")} value={1} />
           </Tabs>
 
-          <TabPanel value="0">dingen hier</TabPanel>
-          <TabPanel value="1">dingen hier</TabPanel>
+          <TabPanel value="0">
+            <MessagesTable {...{ messages }} />
+          </TabPanel>
+          <TabPanel value="1">
+            <MessagesTable messages={messages.map((message) => ({ ...message, isRead: false }))} />
+          </TabPanel>
         </TabContext>
       </div>
-
-      <Divider />
 
       <div className={styles.cases}>
         <div className={styles.casesHeading}>
@@ -92,4 +95,12 @@ const cases: ICaseTableItem[] = [
     status: "Under consideration",
     date: "3 mei 2022",
   },
+];
+
+const messages: IMessageTableItem[] = [
+  { organisation: "Buren", date: "3 mei 2022", id: "ceb3b7cb-0da2-4fcb-a1a5-69ed38852a28" },
+  { organisation: "Utrecht", date: "12 oktober 2021", id: "f9aa6486-2ee9-4fc6-9c49-015ab4eb2afd" },
+  { organisation: "Utrecht", date: "10 oktober 2021", id: "28661a53-5bb5-48e3-b055-7e2822e4f70f" },
+  { organisation: "Buren", date: "15 september 2021", id: "7f4ca6d7-4b7e-4e3a-9b59-89087e6b1dab" },
+  { organisation: "Buren", date: "13 september 2021", id: "60aad570-71cd-4fcc-b441-3dacbed4619e" },
 ];
