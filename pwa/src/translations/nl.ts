473cb92755b4538546bb1d04219d82712083e829
Remko <remko@conduction.nl>
2022-05-05 13:54:42 +0200

    removed unused translations

--- pwa/src/translations/nl.ts
+++ pwa/src/translations/nl.ts
@@ -20,8 +20,6 @@ export const nl = {
   "My messages": "Mijn berichten",
   "Closed cases": "Gesloten zaken",
   case: "Zaak",
-  Themes: "Thema's",
-  Forms: "Formulieren",
   "My account": "Mijn account",
   "The Hague": "Den Haag",
   "Go to website": "Ga naar denhaag.nl",
