a88598de9dc1e67d8bff06c733636cd5c7428fc2
Lennart <66552932+lencodes@users.noreply.github.com>
2022-05-06 09:48:40 +0200

    REFACTOR LandingTemplate layout

--- pwa/src/templates/landing/LandingTemplate.tsx
+++ pwa/src/templates/landing/LandingTemplate.tsx
@@ -4,19 +4,23 @@ import { Card, Heading3, Paragraph } from "@gemeente-denhaag/components-react";
 import * as styles from "./LandingTemplate.module.css";
 import { navigate } from "gatsby";
 import { t } from "i18next";
+
 export const LandingTemplate: React.FC = () => {
   return (
     <Container>
       <div className={styles.container}>
+        <div className={styles.introduction}>
           <Heading3>{t("Log in to MijnDenHaag")}</Heading3>
           <Paragraph>
             {t(
-              "In the personal online environment MijnDenHaag you can easily, quickly and safely arrange matters with the municipality. " +
-              "Bewoners kunnen bijvoorbeeld een verhuizing doorgeven of extra parkeertegoed kopen via DigiD. " +
-              "Bedrijven kunnen eenvoudig online zaken regelen via eHerkenning.",
+              "In the personal online environment MijnDenHaag you can easily, quickly and safely arrange matters with the municipality. For example, residents can report a move or buy extra parking credit via DigiD. Companies can easily arrange business online via eHerkenning.",
             )}
           </Paragraph>
+        </div>
+
+        <div className={styles.loginCards}>
           <Card title={t("Login")} onClick={() => navigate("/login")} subTitle={t("Account")} />
+        </div>
       </div>
     </Container>
   );
