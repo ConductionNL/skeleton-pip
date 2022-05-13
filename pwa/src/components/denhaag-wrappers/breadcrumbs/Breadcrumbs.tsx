c1a905d08f325540b86ed1cd620aa84eeb5b0347
Lennart <66552932+lencodes@users.noreply.github.com>
2022-05-13 11:57:04 +0200

    REFACTOR Breadcrumbs wrapper to allow for dynamic content

--- pwa/src/components/denhaag-wrappers/breadcrumbs/Breadcrumbs.tsx
+++ pwa/src/components/denhaag-wrappers/breadcrumbs/Breadcrumbs.tsx
@@ -6,93 +6,84 @@
  * Note: we do not use css modules here due to this component being a wrapper
  */
 
+import { Link } from "gatsby";
 import * as React from "react";
 import "./Breadcrumbs.css";
 
-export const Breadcrumbs: React.FC = () => {
+interface BreadcrumbsProps {
+  crumbs: {
+    pathname: string;
+    crumbLabel: string;
+  }[];
+}
+
+export const Breadcrumbs: React.FC<BreadcrumbsProps> = ({ crumbs }) => {
   return (
     <nav aria-label="Breadcrumb" className="denhaag-breadcrumb">
       <ol className="denhaag-breadcrumb__list" itemScope itemType="https://schema.org/BreadcrumbList">
-        <li
-          className="denhaag-breadcrumb__item"
-          itemProp="itemListElement"
-          itemScope
-          itemType="https://schema.org/ListItem"
-        >
-          <a className="denhaag-breadcrumb__link" href="https://example.com/" itemProp="item">
-            <span className="denhaag-breadcrumb__text" itemProp="name">
-              Home
-            </span>
-            <svg
-              aria-hidden="true"
-              className="denhaag-icon"
-              fill="none"
-              focusable="false"
-              height="1em"
-              shape-rendering="auto"
-              viewBox="0 0 24 24"
-              width="1em"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M9.293 18.707a1 1 0 010-1.414L14.586 12 9.293 6.707a1 1 0 011.414-1.414l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414 0z"
-                fill="currentColor"
-              />
-            </svg>
-            <meta content="1" itemProp="position" />
-          </a>
-        </li>
+        {crumbs.map((crumb, idx) => {
+          if (crumbs.length !== idx + 1) {
+            return <CrumbItem key={idx} {...crumb} {...{ idx }} />;
+          }
 
-        <li
-          className="denhaag-breadcrumb__item"
-          itemProp="itemListElement"
-          itemScope
-          itemType="https://schema.org/ListItem"
-        >
-          <a className="denhaag-breadcrumb__link" href="https://example.com/a/" itemProp="item">
-            <span className="denhaag-breadcrumb__text" itemProp="name">
-              Afval
-            </span>
-            <svg
-              aria-hidden="true"
-              className="denhaag-icon"
-              fill="none"
-              focusable="false"
-              height="1em"
-              shape-rendering="auto"
-              viewBox="0 0 24 24"
-              width="1em"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M9.293 18.707a1 1 0 010-1.414L14.586 12 9.293 6.707a1 1 0 011.414-1.414l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414 0z"
-                fill="currentColor"
-              />
-            </svg>
-            <meta content="2" itemProp="position" />
-          </a>
-        </li>
-
-        <li
-          aria-current="page"
-          className="denhaag-breadcrumb__item"
-          itemProp="itemListElement"
-          itemScope
-          itemType="https://schema.org/ListItem"
-        >
-          <a
-            aria-current="page"
-            className="denhaag-breadcrumb__link denhaag-breadcrumb__link--current"
-            href="https://example.com/a/b/"
-            itemProp="item"
-          >
-            <span className="denhaag-breadcrumb__text" itemProp="name">
-              Kliko's
-            </span>
-            <meta content="3" itemProp="position" />
-          </a>
-        </li>
+          return <LastCrumbItem key={idx} {...crumb} {...{ idx }} />;
+        })}
       </ol>
     </nav>
   );
 };
+
+interface CrumbItemProps {
+  pathname: string;
+  crumbLabel: string;
+  idx: number;
+}
+
+const CrumbItem: React.FC<CrumbItemProps> = ({ pathname, crumbLabel, idx }) => (
+  <li className="denhaag-breadcrumb__item" itemProp="itemListElement" itemScope itemType="https://schema.org/ListItem">
+    <Link className="denhaag-breadcrumb__link" to={pathname} itemProp="item">
+      <span className="denhaag-breadcrumb__text" itemProp="name">
+        {crumbLabel}
+      </span>
+      <svg
+        aria-hidden="true"
+        className="denhaag-icon"
+        fill="none"
+        focusable="false"
+        height="1em"
+        shape-rendering="auto"
+        viewBox="0 0 24 24"
+        width="1em"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M9.293 18.707a1 1 0 010-1.414L14.586 12 9.293 6.707a1 1 0 011.414-1.414l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414 0z"
+          fill="currentColor"
+        />
+      </svg>
+      <meta content={idx.toString()} itemProp="position" />
+    </Link>
+  </li>
+);
+
+const LastCrumbItem: React.FC<CrumbItemProps> = ({ pathname, crumbLabel, idx }) => (
+  <li
+    aria-current="page"
+    className="denhaag-breadcrumb__item"
+    itemProp="itemListElement"
+    itemScope
+    itemType="https://schema.org/ListItem"
+  >
+    <Link
+      aria-current="page"
+      className="denhaag-breadcrumb__link denhaag-breadcrumb__link--current"
+      to={pathname}
+      itemProp="item"
+    >
+      <span className="denhaag-breadcrumb__text" itemProp="name">
+        {crumbLabel}
+      </span>
+      <meta content={idx.toString()} itemProp="position" />
+    </Link>
+  </li>
+);
