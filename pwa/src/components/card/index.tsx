f8db27d843a7edd69feb9b7cfa264cd366d5c6dc
Lennart <66552932+lencodes@users.noreply.github.com>
2022-05-24 16:30:04 +0200

    ADD RichContentCard component

--- pwa/src/components/card/index.tsx
+++ pwa/src/components/card/index.tsx
@@ -1,5 +1,6 @@
 import { DownloadCard } from "./downloadCard/DownloadCard";
 import { HorizontalImageCard } from "./HorizontalImageCard/HorizontalImageCard";
 import { ImageAndDetailsCard } from "./imageAndDetailsCard/ImageAndDetailsCard";
+import { RichContentCard } from "./RichContentCard/RichContentCard";
 
-export { DownloadCard, HorizontalImageCard, ImageAndDetailsCard };
+export { DownloadCard, HorizontalImageCard, ImageAndDetailsCard, RichContentCard };
