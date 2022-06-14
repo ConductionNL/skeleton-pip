27ee10b2580331bea9614fdbe89717c3cd568ccd
Lennart <66552932+lencodes@users.noreply.github.com>
2022-06-14 11:50:59 +0200

    ADD getOne to message resource

--- pwa/src/apiService/resources/message.ts
+++ pwa/src/apiService/resources/message.ts
@@ -8,6 +8,12 @@ export default class Message {
     this._instance = _instance;
   }
 
+  public getOne = async (messageId: string): Promise<any> => {
+    const { data } = await Send(this._instance, "GET", `/contactmomenten/${messageId}`);
+
+    return data;
+  };
+
   public getAll = async (): Promise<any> => {
     const {
       data: { results },
